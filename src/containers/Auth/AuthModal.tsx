ae1895c12c257f65195bb828c52e7db3abe22adf
Pavel Nagorsky <pavelnagorsky@mail.ru>
2023-08-29 23:08:18 +0300

    added initial auth modal

---
+++ src/containers/Auth/AuthModal.tsx
@@ -0,0 +1,60 @@
+import { Button } from "@/components/UI/Button/Button";
+import Dialog from "@mui/material/Dialog/Dialog";
+import DialogActions from "@mui/material/DialogActions/DialogActions";
+import DialogContent from "@mui/material/DialogContent/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle/DialogTitle";
+import Slide from "@mui/material/Slide/Slide";
+import { TransitionProps } from "@mui/material/transitions/transition";
+import { forwardRef, ReactElement, Ref, useState } from "react";
+
+const Transition = forwardRef(function Transition(
+  props: TransitionProps & {
+    children: ReactElement<any, any>;
+  },
+  ref: Ref<unknown>
+) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+const AuthModal = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Button variant="outlined" onClick={handleClickOpen}>
+        Slide in alert dialog
+      </Button>
+      <Dialog
+        scroll={"paper"}
+        open={open}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={handleClose}
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogTitle>{"Use Google's location service?"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-slide-description">
+            Let Google help apps determine location. This means sending
+            anonymous location data to Google, even when no apps are running.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Disagree</Button>
+          <Button onClick={handleClose}>Agree</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+};
+
+export default AuthModal;
