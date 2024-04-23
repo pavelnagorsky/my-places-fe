0c14e837979e684173c5c98f6fad83cd5cd84e4c
Pavel Nagorsky <pavelnagorsky@asist-lab.com>
2024-04-23 10:36:14 +0300

    fixed envs

--- src/shared/Environment.ts
+++ src/shared/Environment.ts
@@ -1,10 +1,12 @@
 export class Environment {
-  static readonly domain = process.env["DOMAIN"] ?? "my-places.by";
+  static readonly domain = process.env["NEXT_PUBLIC_DOMAIN"] ?? "my-places.by";
 
-  static readonly googleMapsKey = process.env["GOOGLE_API_KEY"] ?? "";
+  static readonly googleMapsKey =
+    process.env["NEXT_PUBLIC_GOOGLE_API_KEY"] ?? "";
 
   static readonly backendBaseUrl =
-    process.env["BACKEND_BASE_URL"] ?? "http://localhost:3000";
+    process.env["NEXT_PUBLIC_BACKEND_BASE_URL"] ?? "http://localhost:3000";
 
-  static readonly email = process.env["EMAIL"] ?? "support@my-places.by";
+  static readonly email =
+    process.env["NEXT_PUBLIC_EMAIL"] ?? "support@my-places.by";
 }
