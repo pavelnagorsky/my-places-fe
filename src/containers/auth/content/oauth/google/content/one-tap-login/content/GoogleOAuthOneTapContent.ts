ead1bf217373bcddd41c7808c46c311960250bf0
Pavel Nagorsky <pavelnagorsky@asist-lab.com>
2025-05-26 18:08:17 +0300

    integrated google one-tap auth

--- src/containers/auth/content/oauth/google/content/one-tap-login/content/GoogleOAuthOneTapContent.ts
+++ src/containers/auth/content/oauth/google/content/one-tap-login/content/GoogleOAuthOneTapContent.ts
@@ -1,9 +1,18 @@
 import { useGoogleOneTapLogin } from "@react-oauth/google";
+import authService from "@/services/auth-service/auth.service";
+import { GoogleOauthTypesEnum } from "@/services/auth-service/enums/google-oauth-type.enum";
 
 const GoogleOAuthOneTapContent = () => {
   useGoogleOneTapLogin({
     onSuccess: (credentialResponse) => {
       console.log("one-tap", credentialResponse);
+      authService
+        .googleOAuth({
+          type: GoogleOauthTypesEnum.ONE_TAP,
+          oneTapCode: credentialResponse.credential,
+        })
+        .then(() => {})
+        .catch(() => {});
     },
     onError: () => {
       console.log("Login Failed");
